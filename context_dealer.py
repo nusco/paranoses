b328dec2eccad95f6321afaa18796daa39dc8526
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2015-04-09 15:39:40 +0200

    Clean up more

--- context_dealer.py
+++ context_dealer.py
@@ -3,16 +3,16 @@ import multiprocessing
 import os
 import time
 
-class Keymaster(multiprocessing.Process):
+class ContextDealer(multiprocessing.Process):
   def __init__(self, queue_in, queue_out):
     multiprocessing.Process.__init__(self, name="keymaster")
     self.queue_in = queue_in
     self.queue_out = queue_out
-    self.locked_contexts = {"X" : False, "Y" : False, "Z" : False, "W" : False, "XX" : False, "YY" : False, "ZZ" : False, "WW" : False}
+    self.locked_contexts = {"X" : False, "Y" : False, "Z" : False, "W": False}
     self.access_lock = threading.Lock()
 
   def run(self):
-    print "Keymaster process started with id " + str(os.getpid())
+    print "ContextDealer process started with id " + str(os.getpid())
 
     while True:
         request = self.queue_in.get()
@@ -25,14 +25,14 @@ class Keymaster(multiprocessing.Process):
   def _return_context(self):
       for context, locked in self.locked_contexts.items():
           if not locked:
-              print "Keymaster got a locking request. Returning context: " + context
+              print "ContextDealer got a locking request. Returning context: " + context
 
               self.locked_contexts[context] = True
               self.queue_out.put(context)
               return
-      raise Exception("No available context left. Go easy on those parallel processes.")
+      raise Exception("No available context left. You cannot have more parallel test processes than you have contexts.")
 
   def _release_context(self, context):
-      print "Keymaster got a release request. Releasing context: " + context
+      print "ContextDealer got a release request. Releasing context: " + context
 
       self.locked_contexts[context] = False
--- paranoses.py
+++ paranoses.py
@@ -3,39 +3,39 @@ import multiprocessing
 import os
 import time
 import random
-import keymaster
+import context_dealer
 
-_queue_to_keymaster = None
-_queue_from_keymaster = None
-_keymaster = None
-_keymaster_process_lock = multiprocessing.Lock()
+_queue_to_context_dealer = None
+_queue_from_context_dealer = None
+_context_dealer = None
+_context_dealer_process_lock = multiprocessing.Lock()
 
 class ParallelTest(unittest.TestCase):
     @classmethod
     def setup_class(cls):
-        with _keymaster_process_lock:
-            global _keymaster
-            global _queue_to_keymaster
-            global _queue_from_keymaster
-            if _keymaster == None:
-                print "Starting keymaster in main process (" + str(os.getpid()) + ")"
+        with _context_dealer_process_lock:
+            global _context_dealer
+            global _queue_to_context_dealer
+            global _queue_from_context_dealer
+            if _context_dealer == None:
+                print "Starting context_dealer in main process (" + str(os.getpid()) + ")"
                 
-                _queue_to_keymaster = multiprocessing.Queue()
-                _queue_from_keymaster = multiprocessing.Queue()
-                _keymaster = keymaster.Keymaster(_queue_to_keymaster, _queue_from_keymaster)
-                _keymaster.daemon = True
-                _keymaster.start()
+                _queue_to_context_dealer = multiprocessing.Queue()
+                _queue_from_context_dealer = multiprocessing.Queue()
+                _context_dealer = context_dealer.ContextDealer(_queue_to_context_dealer, _queue_from_context_dealer)
+                _context_dealer.daemon = True
+                _context_dealer.start()
 
     def setUp(self):
         self.test_id = str(random.randint(0, 1000000000)) # TODO: temporary. use a serial that is different for each test instead
         print "Test " + self.test_id + " in process " + str(os.getpid()) + " asks to own a context"
 
-        _queue_to_keymaster.put("GET")
-        self.context = _queue_from_keymaster.get()
+        _queue_to_context_dealer.put("GET")
+        self.context = _queue_from_context_dealer.get()
 
         print "Test " + self.test_id + " in process " + str(os.getpid()) + " acquired context " + self.context + " and can proceed."
         
     def tearDown(self):
-        _queue_to_keymaster.put(self.context)
+        _queue_to_context_dealer.put(self.context)
 
         print "Test " + self.test_id + " in process " + str(os.getpid()) +  " released context " + self.context + "."
