9354c4a7dbb6fea48b1854277dc42072b3072973
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2015-04-09 13:44:59 +0200

    Moving further towards shared state with manager

--- keymaster.py
+++ keymaster.py
@@ -4,31 +4,20 @@ import os
 import time
 
 class Keymaster(multiprocessing.Process):
-  def __init__(self, locking_queue):
+  def __init__(self, contexts_to_owners, bookings):
     multiprocessing.Process.__init__(self, name="keymaster")
-    self.locking_queue = locking_queue
-    self.call_lock = threading.Lock()
-    self.context_locks = {}
+    self.contexts_to_owners = contexts_to_owners
+    self.bookings = bookings
 
   def run(self):
     print "Keymaster process started with id " + str(os.getpid())
     while True:
-      command = self.locking_queue.get()
-      print "Keymaster got command: " + command
+      for booker, context in self.bookings.items():
+          if self._is_available(context):
+              del self.bookings[booker]
+              self.contexts_to_owners[context] = booker
+              print "Keymaster allowed " + booker + " to own context " + context
+      time.sleep(1)
 
-  def lock(self, context):
-    print "Keymaster acquiring lock of " + context
-    self.lock_of[context].acquire(3600)
-    print "Keymaster acquired lock of " + context
-
-  def release(self, context):
-    print "Keymaster releasing lock of " + context
-    self.lock_of[context].release()
-    print "Keymaster released lock of " + context
-
-  def _lock_of(self, context):
-    with self.call_lock:
-      if not self.context_locks.has_key(context):
-        self.context_locks[context] = threading.Lock()
-      return self.context_locks[context]
-
+  def _is_available(self, context):
+    return not self.contexts_to_owners.has_key(context)
--- paranoses.py
+++ paranoses.py
@@ -1,9 +1,12 @@
 import unittest
 import multiprocessing
 import os
+import time
+import random
 import keymaster
-import keymaster_commands
 
+_keymaster_contexts_to_owners = None
+_keymaster_bookings = None
 _keymaster = None
 _keymaster_locking_queue = multiprocessing.Queue()
 _keymaster_process_lock = multiprocessing.Lock()
@@ -12,27 +15,39 @@ class ParallelTest(unittest.TestCase):
     @classmethod
     def setup_class(cls):
         with _keymaster_process_lock:
-            global _keymaster
-            if _keymaster == None:
-                print "Starting keymaster from main process (" + str(os.getpid()) + ")"
-                _keymaster = keymaster.Keymaster(_keymaster_locking_queue)
+            global _keymaster_contexts_to_owners
+            global _keymaster_bookings
+            if _keymaster_contexts_to_owners == None:
+                print "Starting keymaster in main process (" + str(os.getpid()) + ")"
+                manager = multiprocessing.Manager()
+                _keymaster_contexts_to_owners = manager.dict()
+                _keymaster_bookings = manager.dict()
+                _keymaster = keymaster.Keymaster(_keymaster_contexts_to_owners, _keymaster_bookings)
                 _keymaster.daemon = True
                 _keymaster.start()
-
+        
     @classmethod
     def teardown_class(cls):
         pass # TODO: the daemon terminates on its own, but do we need to do anything else here?
 
     def setUp(self):
+        self.test_id = str(random.randint(0, 1000000000)) # TODO: temporary. use a serial that is different for each test instead
         context = "X" # temporary catch-all value
-        command = keymaster_commands.LockCommand(context)
-        _keymaster_locking_queue.put(command)
-        print "Process " + str(os.getpid()) + " send a locking command. Waiting on event..."
-        # TODO: wait until we have permission to run
-        print "...event triggered. Process " + str(os.getpid()) + " owns context " + context + "."
+        print "Test " + self.test_id + " in process " + str(os.getpid()) + " asks to own context " + context
+        _keymaster_bookings[self.test_id] = context
 
+        # TODO: use polling for now. Maybe events later?
+        while not self._is_owner_of(context):
+            time.sleep(0.1)
+
+        print "Test " + self.test_id + " in process " + str(os.getpid()) + " acquired context " + context + " and can proceed."
+
+    def _is_owner_of(self, context):
+        if not _keymaster_contexts_to_owners.has_key(self.test_id):
+            return False
+        return _keymaster_contexts_to_owners[self.test_id] == context
+        
     def tearDown(self):
         context = "X" # temporary catch-all value
-        command = keymaster_commands.ReleaseCommand(context)
-        _keymaster_locking_queue.put(command)
-        print "Process " + str(os.getpid()) + " sent a release command."
+        _keymaster_contexts_to_owners.remove(self.test_id)
+        print "Test " + self.test_id + " in process " + str(os.getpid()) +  " released context " + context + "."
